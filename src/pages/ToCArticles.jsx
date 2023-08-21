4e398183b0cb3729d23fa8c1fc622ff1c109f2cd
Tyler Smith <tylersmith4596@gmail.com>
2023-08-20 21:29:30 -0700

    fixed a couple things

--- src/pages/ToCArticles.jsx
+++ src/pages/ToCArticles.jsx
@@ -1,19 +1,26 @@
+import { useState } from 'react';
 import { Link } from "react-router-dom";
 import articles from './articles.json';
 import '../styles/ToC.css'
 
 function Articles() {
+    const [showSection, setShowSection] = useState(Array(articles["Table of Contents"].length).fill(false));
+
     return (
         <div className="table-of-content">
             <h1>Articles</h1>
             <p>Welcome to the articles page</p>
             <div className="articles-list">
-                {articles["Table of Contents"].map(section => (
-                    <div key={section.SectionTitle}>
-                        <h2>{section.SectionTitle}</h2>
-                        {section.Items.map(item => (
+                {articles["Table of Contents"].map((section, index) => (
+                    <div className="SectionTitle" key={section.SectionTitle}>
+                        <h2 onClick={() => {
+                            const newShowSection = [...showSection];
+                            newShowSection[index] = !newShowSection[index];
+                            setShowSection(newShowSection);
+                        }}>{section.SectionTitle} <span className={showSection[index] ? "up" : "down"}>^</span></h2>
+                        {showSection[index] && section.Items.map(item => (
                             item.pageID ? (
-                                <Link key={item.pageID} to={`/articles/${item.pageID}`}>
+                                <Link className="" key={item.pageID} to={`/articles/${item.pageID}`}>
                                     <div className="item">{item.pageTitle}</div>
                                 </Link>
                             ) : (
