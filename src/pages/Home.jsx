61a6d9e19e4b578c3055f4b447a2082b52268636
Tyler Smith <tylersmith4596@gmail.com>
2023-08-20 21:05:30 -0700

    will be attempting to recreate the iphone support section from apple you can use this too just follow the rule on the home page and start making markdown file for each page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
 // create a basic home page component
+import React from "react";
+import TextToJson from "../components/TextToJSON";
+
 function Home() {
     return (
     <div>
         <h1>Home</h1>
         <p>Welcome to the home page</p>
+        <TextToJson />
     </div>
     );
 }
--- src/pages/ToCArticles.jsx
+++ src/pages/ToCArticles.jsx
@@ -4,18 +4,36 @@ import '../styles/ToC.css'
 
 function Articles() {
     return (
-    <div className="table-of-content">
-        <h1>Articles</h1>
-        <p>Welcome to the articles page</p>
-        <div className="articles-list">
-          {articles.map(article => (
-            <Link key={article.id} to={`/articles/${article.id}`}>
-              <div className="item">{article.title}</div>
-            </Link>
-          ))}
+        <div className="table-of-content">
+            <h1>Articles</h1>
+            <p>Welcome to the articles page</p>
+            <div className="articles-list">
+                {articles["Table of Contents"].map(section => (
+                    <div key={section.SectionTitle}>
+                        <h2>{section.SectionTitle}</h2>
+                        {section.Items.map(item => (
+                            item.pageID ? (
+                                <Link key={item.pageID} to={`/articles/${item.pageID}`}>
+                                    <div className="item">{item.pageTitle}</div>
+                                </Link>
+                            ) : (
+                                <div key={item.SectionTitle}>
+                                    <h3>{item.SectionTitle}</h3>
+                                    {item.Items.map(subItem => (
+                                        subItem.pageID ? (
+                                            <Link key={subItem.pageID} to={`/articles/${subItem.pageID}`}>
+                                                <div className="item">{subItem.pageTitle}</div>
+                                            </Link>
+                                        ) : null
+                                    ))}
+                                </div>
+                            )
+                        ))}
+                    </div>
+                ))}
+            </div>
         </div>
-    </div>
-);
+    );
 };
 
 export default Articles;
