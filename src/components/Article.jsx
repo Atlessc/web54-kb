e0d1fceb9541a370ba4a1476b07c14a7b8d518e9
Tyler Smith <tylersmith4596@gmail.com>
2023-09-15 10:35:43 -0700

    safv

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -5,77 +5,45 @@ import { useState, useEffect } from 'react';
 import Articles from "../data/articles-map.json";
 import '../styles/Article.css';
 
-const data = Articles;
-const { id } = useParams();
-const [articleMarkdown, setArticleMarkdown] = useState('');
-const [ticketMarkdown, setTicketMarkdown] = useState('');
-const [showTicketInfo, setShowTicketInfo] = useState(false);
-const [article, setArticle] = useState('');
-
-useEffect(() => {
-  // Find the article with the matching id in the JSON data
-  const foundArticle = data[id];
-
-  // Update the article state
-  setArticle(foundArticle);
-
-  // Fetch article content
-  fetch(`/Articles/${id}`)
-    .then((response) => response.text())
-    .then((text) => {
-      // Set the markdown state
-      setArticleMarkdown(<ReactMarkdown className='markdown'>{text}</ReactMarkdown>);
-    });
-}, [id]);
-
-function fetchTixInfo() {
-    // Fetch ticket info
-    fetch(`/TicketInfo/${article.TicketInfoID}`)
-      .then((response) => response.text())
-      .then((text) => {
-        // Set the ticket markdown state
-        setTicketMarkdown(<ReactMarkdown className='markdown'>{text}</ReactMarkdown>);
-      });
-  };
-
 function Article() {
 
   const data = Articles;
-const { id } = useParams();
-const [articleMarkdown, setArticleMarkdown] = useState('');
-const [ticketMarkdown, setTicketMarkdown] = useState('');
-const [showTicketInfo, setShowTicketInfo] = useState(false);
-const [article, setArticle] = useState('');
 
-useEffect(() => {
-  // Find the article with the matching id in the JSON data
-  const foundArticle = data[id];
+  const { id } = useParams();
+  const [articleMarkdown, setArticleMarkdown] = useState('');
+  const [ticketMarkdown, setTicketMarkdown] = useState('');
+  const [showTicketInfo, setShowTicketInfo] = useState(false);
+  const [article, setArticle] = useState('');
 
-  // Update the article state
-  setArticle(foundArticle);
-
-  // Fetch article content
-  fetch(`/Articles/${id}`)
-    .then((response) => response.text())
-    .then((text) => {
-      // Set the markdown state
-      setArticleMarkdown(<ReactMarkdown className='markdown'>{text}</ReactMarkdown>);
-    });
-}, [id]);
-
-function fetchTixInfo() {
-    // Fetch ticket info
-    fetch(`/TicketInfo/${article.TicketInfoID}`)
+  useEffect(() => {
+    // Find the article with the matching id in the JSON data
+    const foundArticle = data[id];
+  
+    // Update the article state
+    setArticle(foundArticle);
+  
+    // Fetch article content
+    fetch(`/Articles/${id}`)
       .then((response) => response.text())
       .then((text) => {
-        // Set the ticket markdown state
-        setTicketMarkdown(<ReactMarkdown className='markdown'>{text}</ReactMarkdown>);
+        // Set the markdown state
+        setArticleMarkdown(<ReactMarkdown className='markdown'>{text}</ReactMarkdown>);
       });
-  };
+  }, [id]);
+  
+  useEffect(() => {
+    if (article) {
+      // Fetch ticket info
+      fetch(`/TicketInfo/${article.TicketInfoID}`)
+        .then((response) => response.text())
+        .then((text) => {
+          // Set the ticket markdown state
+          setTicketMarkdown(<ReactMarkdown className='markdown'>{text}</ReactMarkdown>);
+        });
+    }
+  }, [article]);
 
   const handleButtonClick = () => {
-    // Fetch ticket info
-    fetchTixInfo();
     setShowTicketInfo(!showTicketInfo);
   };
 
