82f1f6157206622119f762df254586c719050808
Tyler Smith <tylersmith4596@gmail.com>
2023-09-15 09:43:52 -0700

    df

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useParams } from 'react-router-dom';
 import '../styles/Article.css';
 import ArticleText from './ArticleText'; // import the ArticleText component
 import TicketInfoText from './TicketInfoText'; // import the TicketInfoText component
 
 function Article() {
 
+  const { id } = useParams();
+  
   const [showTicketInfo, setShowTicketInfo] = useState(false);
 
   const handleButtonClick = () => {
@@ -19,6 +22,9 @@ function Article() {
       </button>
       {showTicketInfo && (
         <div>
+          <h2>Still not working?</h2>
+          {/* Render the information needed for the ticket here */}
+          <p>Ticket Information for Article {id}</p>
           <TicketInfoText /> {/* use the TicketInfoText component */}
         </div>
       )}
