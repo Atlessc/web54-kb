34e15ce02b26cdc3700b19ba5330c1e209dd4c09
Tyler Smith <tylersmith4596@gmail.com>
2023-09-15 09:42:16 -0700

    added a few console logs for error catching

--- src/components/TicketInfoText.jsx
+++ src/components/TicketInfoText.jsx
@@ -11,15 +11,20 @@ export default function TicketInfoText() {
   const ticketInfoID = useStore(state => state.ticketInfoID);
   const [ticketInfoText, setTicketInfoText] = useState('')
 
+  console.log('Article ID from store:', articleID); // log the article ID
+
   useEffect(() => {
     const ticketInfoID = data[articleID].TicketInfoID;
+    console.log('Setting ticket info ID in store:', ticketInfoID); // log before setting the ticket info ID
     setTicketInfo(ticketInfoID);
   }, [articleID]);
 
   useEffect(() => {
     async function fetchTicketInfo() {
+      console.log('Fetching ticket info:', ticketInfoID); // log before fetching the ticket info
       const response = await fetch(`/TicketInfo/TixInfo${ticketInfoID}.md`);
       const text = await response.text();
+      console.log('Fetched ticket info text:', text); // log the fetched text
       setTicketInfoText(text);
     }
 
