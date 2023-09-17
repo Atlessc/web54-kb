0069a2d65f2c837019a3f9aeff8ce0d11c14f3ab
Tyler Smith <tylersmith4596@gmail.com>
2023-09-16 19:49:02 -0700

    drh

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,25 +1,49 @@
-// Article.js
-import React, { useEffect, useState } from 'react';
-import { useParams, Link } from 'react-router-dom';
+import React from 'react';
+import { useParams, Link } from 'react-router-dom'; // import Link
+import ReactMarkdown from 'react-markdown';
+import { useState, useEffect } from 'react';
 import Articles from "../data/articles-map.json";
+import '../styles/Article.css';
+import useStore from '../store';
+
 
 function Article() {
+
+  const data = Articles;
+
   const { id } = useParams();
-  const [article, setArticle] = useState(null);
+  const [articleMarkdown, setArticleMarkdown] = useState('');
+  const [article, setArticle] = useState('');
+  const articleID = useStore(state => state.articleID);
+  const setArticleID = useStore(state => state.setArticleID);
+  const ticketInfoID = useStore(state => state.ticketInfoID);
+  const setTicketInfoID = useStore(state => state.setTicketInfoID);
+
+   // log the ticket info ID
+
+
 
   useEffect(() => {
-    const articleData = Articles.Articles.find(article => article.pageID === id);
-    setArticle(articleData);
-  }, [id]);
+  
+    // Fetch article content
+    fetch(`/Articles/${id}`)
+      .then((response) => response.text())
+      .then((text) => {
+        // Set the markdown state
+        setArticleMarkdown(text); // log the fetched text
+      });
+    }, [id]);
 
-  if (!article) {
-    return <div>Loading...</div>;
-  }
+    console.log(`Setting ticket info ID: ${ticketInfoID}`); // log before setting the ticket info ID // log before setting the article ID
+    console.log(`Article ID: ${articleID}`); // log the article ID
+    console.log(`Ticket Info ID: ${ticketInfoID}`);
 
   return (
     <div>
-      <h1>{article.pageTitle}</h1>
-      <Link to={`/ticket-info/${article.TicketInfoID}`}>Ticket Info</Link>
+      {/* <ReactMarkdown>{articleMarkdown}</ReactMarkdown> */}
+      <Link to={`/ticket-info/${ticketInfoID}`}>
+        Ticket Info
+      </Link>
     </div>
   );
 }
