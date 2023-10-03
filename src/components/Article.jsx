9e552a1eb005373f58ee2a638950745e9de5f25b
Tyler Smith <tylersmith4596@gmail.com>
2023-10-03 09:51:51 -0700

    testing if this works to display the articles and get the ticket info

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -5,7 +5,6 @@ import { useState, useEffect } from 'react';
 import Articles from "../data/Article-Objects.json";
 import '../styles/Article.css';
 
-
 function Article() {
 
   const { id } = useParams();
@@ -13,7 +12,6 @@ function Article() {
   const [article, setArticle] = useState('');
 
   useEffect(() => {
-  
     // Fetch article content
     fetch(`/Articles/${id}`)
       .then((response) => response.text())
@@ -29,26 +27,33 @@ function Article() {
       // Get the id from the props
       const { id } = props;
 
-        // Find the item in the Articles array that has the same pageID as the id
-        const item = Articles.find((item) => item.pageID === id);
-        console.log('item:', item);
-      
-        // If the item is found, return a Link element to its TicketInfoID. Otherwise, return a message.
-        if (item) {
-          return <Link to={`/ticket-info/${item.TicketInfoID}`} className='button'>
-          Ticket Info
-        </Link>;
-        } else {
-          return <p>no ticket info needed or not determined</p>;
-        }
+      // Find the item in the Articles array that has the same pageID as the id
+      const item = Articles.find((item) => item.pageID === id);
 
+      let ticketInfoId;
+      // If the item is found, check if it has a direct TicketInfoID property
+      if (item && item.TicketInfoID) {
+        ticketInfoId = item.TicketInfoID;
+      } 
+      // If not, check if it has an attributes array and use the TicketInfoID from the first attribute
+      else if (item && item.attributes && item.attributes.length > 0 && item.attributes[0].TicketInfoID) {
+        ticketInfoId = item.attributes[0].TicketInfoID;
       }
+
+      // If ticketInfoId is found, return a Link element to its TicketInfoID. Otherwise, return a message.
+      if (ticketInfoId) {
+        return <Link to={`/ticket-info/${ticketInfoId}`} className='button'>
+        Ticket Info
+        </Link>;
+      } else {
+        return <p>no ticket info needed or not determined</p>;
+      }
+    }
 
   return (
     <div className="markdown">
       <GetTicketInfoID id={id} />
       <ReactMarkdown className='Article'>{articleMarkdown}</ReactMarkdown>
-      {/* Pass the id as a prop to the GetTicketInfoID function */}
     </div>
   );
 }
