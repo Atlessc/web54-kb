5dec459c057a2b6b931c9fe69ce8aa96ffd8e198
Tyler Smith <tylersmith4596@gmail.com>
2024-01-16 21:38:05 -0800

    Refactor ArticleObjectGenerator.jsx by removing unnecessary code and handling missing '@' symbol error

--- src/components/admin/ArticleObjectGenerator.jsx
+++ src/components/admin/ArticleObjectGenerator.jsx
@@ -15,20 +15,17 @@ function ArticleObjectGenerator() {
     let ticketInfoID = '';
     
     const articles = titles.map((title, index) => {
-      // create a for loop for each index create a new object
       const pageID = `WD${(index+1).toString().padStart(6, "0")}.md`;
       const ticketInfoMatch = title.match(/@(\d+)/);
       if (ticketInfoMatch) {
         const ticketNumber = parseInt(ticketInfoMatch[1], 10);
         ticketInfoID = ticketNumber === 0 ? '' : `TixInfo${ticketNumber.toString().padStart(2, '0')}.md`;
       } else {
-        // Handle error for missing '@' symbol
         console.error(`Error: '@' symbol is missing in the title "${title}"`);
       }
 
       const pageTitle = title.split('@')[0].replace(/^\//, '').trim();
       return {
-        // make the page id a dynamic key  of the data based on the index
         [pageID]: {
         "pageTitle": pageTitle,
         "roleLvAccess": [
